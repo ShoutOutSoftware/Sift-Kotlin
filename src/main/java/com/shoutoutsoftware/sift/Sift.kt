1190406df5cc74953c9865e2cc1857059634e014
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2017-11-13 21:46:01 +0530

    - Changed individual methods to read number types to just one that reads Number - Added read HashMap - Added read list methods for Numbers, Strings, and HashMaps

--- src/main/java/com/shoutoutsoftware/sift/Sift.kt
+++ src/main/java/com/shoutoutsoftware/sift/Sift.kt
@@ -1,7 +1,5 @@
 package com.shoutoutsoftware.sift
 
-import java.util.*
-
 /**
  * Created by Obaid Ahmed Mohammed on 20 September 2017
  * Copyright © 2017 ShoutOut Software. All rights reserved.
@@ -9,39 +7,72 @@ import java.util.*
 
 class Sift {
 
-    companion object {
+    @Throws(SiftException::class)
+    fun readString(map: HashMap<String, Any?>, key: String): String = read(map, key)
 
-        fun readString(map: HashMap<String, Any?>, key: String, defaultValue: String?): String? =
-                read(map, key, defaultValue)
+    fun readString(map: HashMap<String, Any?>, key: String, defaultValue: String?): String? = read(map, key, defaultValue)
 
-        @Throws(SiftException::class)
-        fun readString(map: HashMap<String, Any?>, key: String): String = read(map, key)
+    @Throws(SiftException::class)
+    fun readNumber(map: HashMap<String, Any?>, key: String): Number = read(map, key)
 
-        private inline fun <reified T : Any> read(map: HashMap<String, Any?>, key: String, defaultValue: T?): T? {
-            return try {
-                read(map, key)
-            } catch (e: SiftException) {
-                defaultValue
-            }
+    fun readNumber(map: HashMap<String, Any?>, key: String, defaultValue: Number?): Number? = read(map, key, defaultValue)
+
+    @Throws(SiftException::class)
+    fun readNumberList(map: HashMap<String, Any?>, key: String): List<Number> = read(map, key)
+
+    fun readNumberList(map: HashMap<String, Any?>, key: String, defaultValue: List<Number>?): List<Number>? = read(map, key, defaultValue)
+
+    @Throws(SiftException::class)
+    fun readStringList(map: HashMap<String, Any?>, key: String): List<String> = read(map, key)
+
+    fun readStringList(map: HashMap<String, Any?>, key: String, defaultValue: List<String>?): List<String>? = read(map, key, defaultValue)
+
+    @Throws(SiftException::class)
+    fun readHashMap(map: HashMap<String, Any?>, key: String): HashMap<String, Any?> = read(map, key)
+
+    fun readHashMap(map: HashMap<String, Any?>, key: String, defaultValue: HashMap<String, Any?>?): HashMap<String, Any?>? = read(map, key, defaultValue)
+
+    @Throws(SiftException::class)
+    fun readHashMapList(map: HashMap<String, Any?>, key: String): List<HashMap<String, Any?>> = read(map, key)
+
+    fun readHashMapList(map: HashMap<String, Any?>, key: String, defaultValue: List<HashMap<String, Any?>>?): List<HashMap<String, Any?>>? = read(map, key, defaultValue)
+
+    private inline fun <reified T : Any> read(map: HashMap<String, Any?>, key: String, defaultValue: T?): T? {
+        return try {
+            read(map, key)
+        } catch (e: SiftException) {
+            defaultValue
+        }
+    }
+
+    @Throws(SiftException::class)
+    private inline fun <reified T : Any> read(map: HashMap<String, Any?>, key: String): T {
+        if (map.containsKey(key)) {
+            return parseValue(map[key])
+        } else {
+            throw SiftException("key not found")
+        }
+    }
+
+    @Throws(SiftException::class)
+    private inline fun <reified T : Any> parseValue(value: Any?): T {
+        if (value == null) {
+            throw SiftException("the value is null")
+        } else {
+            return checkValType(value)
+        }
+    }
+
+    @Throws(SiftException::class)
+    private inline fun <reified T : Any> checkValType(value: Any): T {
+
+        if (value is T) {
+            return value
+        } else {
+            throw SiftException("the value type is not the same as the requested one" +
+                    "\nRequested: " + T::class + "\nFound: " + value::class)
         }
 
-        @Throws(SiftException::class)
-        private inline fun <reified T : Any> read(map: HashMap<String, Any?>, key: String): T {
-            if (map.containsKey(key)) {
-                val value = map[key]
-                if (value == null) {
-                    throw SiftException("the value is null")
-                } else {
-                    if (value is T) {
-                        return value
-                    } else {
-                        throw SiftException("the value type is not the same as the requested one")
-                    }
-                }
-            } else {
-                throw SiftException("key not found")
-            }
-        }
     }
 
 }
--- src/test/java/com/shoutoutsoftware/sift/SiftTest.java
+++ src/test/java/com/shoutoutsoftware/sift/SiftTest.java
@@ -4,7 +4,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
 
 
 /**
@@ -14,6 +18,7 @@ import java.util.HashMap;
 
 public class SiftTest {
 
+    private Sift sift = new Sift();
     private final HashMap<String, Object> stringData = new HashMap<>();
 
     @Before
@@ -25,47 +30,98 @@ public class SiftTest {
 
     @Test(expected = SiftException.class)
     public void testThrowsExceptionWhenKeyIsNotFound() throws SiftException {
-        Sift.Companion.readString(stringData, "some random key");
+        sift.readString(stringData, "some random key");
     }
 
     @Test(expected = SiftException.class)
     public void testThrowsExceptionIfValueIsNotAString() throws SiftException {
-        Sift.Companion.readString(stringData, "wrongType");
+        sift.readString(stringData, "wrongType");
     }
 
     @Test(expected = SiftException.class)
     public void testThrowsExceptionIfValueIsNull() throws SiftException {
-        Sift.Companion.readString(stringData, "nullValue");
+        sift.readString(stringData, "nullValue");
     }
 
     @Test
     public void testStringIsReadSuccessfully() throws SiftException {
-        String value = Sift.Companion.readString(stringData, "correctValue");
+        String value = sift.readString(stringData, "correctValue");
         Assert.assertEquals(value, "some string");
     }
 
     @Test
     public void testReturnsDefaultValueWhenKeyIsNotFound() {
-        String value = Sift.Companion.readString(stringData, "some random key", "default val");
+        String value = sift.readString(stringData, "some random key", "default val");
         Assert.assertEquals(value, "default val");
     }
 
     @Test
     public void testReturnsDefaultValueWhenValueIsNotAString() {
-        String value = Sift.Companion.readString(stringData, "wrongType", "default val");
+        String value = sift.readString(stringData, "wrongType", "default val");
         Assert.assertEquals(value, "default val");
     }
 
     @Test
     public void testReturnsDefaultValueWhenValueIsNull() {
-        String value = Sift.Companion.readString(stringData, "nullValue", "default val");
+        String value = sift.readString(stringData, "nullValue", "default val");
         Assert.assertEquals(value, "default val");
     }
 
     @Test
     public void testDefaultValueIsIgnoredWhenStringIsReadSuccessfully() {
-        String value = Sift.Companion.readString(stringData, "correctValue", "default val");
+        String value = sift.readString(stringData, "correctValue", "default val");
         Assert.assertEquals(value, "some string");
+    }
+
+    @Test
+    public void testComplexMap() throws SiftException {
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("null", null);
+        data.put("int", 1);
+        data.put("string", "some string");
+        data.put("float", 2.1);
+        data.put("double", 12.432132213);
+        data.put("intArray", Arrays.asList(1, 2, 3));
+        data.put("stringArray", Arrays.asList("valOne", "valTwo", "valThree"));
+
+        HashMap<String, Object> innerMap1 = new HashMap<>();
+        innerMap1.put("innerString", "inner1Val");
+        innerMap1.put("innerDoubleArray", Arrays.asList(1.2, 32.3, 32.4423));
+        data.put("innerMap", innerMap1);
+
+
+        HashMap<String, Object> arrayMap1 = new HashMap<>();
+        arrayMap1.put("arrayMap1String", "aMS1");
+
+        HashMap<String, Object> arrayMap2 = new HashMap<>();
+        arrayMap2.put("arrayMap2Int", 3);
+
+        HashMap<String, Object> arrayMap3 = new HashMap<>();
+        arrayMap3.put("arrayMap3Array", Arrays.asList("a", "b", "c"));
+
+        data.put("mapList", Arrays.asList(arrayMap1, arrayMap2, arrayMap3));
+
+        assertEquals(null, sift.readString(data, "null", null));
+        assertEquals(1, sift.readNumber(data, "int"));
+        assertEquals("some string", sift.readString(data, "string"));
+        assertEquals(2.1, sift.readNumber(data, "float"));
+        assertEquals(12.432132213, sift.readNumber(data, "double"));
+        assertEquals(Arrays.asList(1, 2, 3), sift.readNumberList(data, "intArray"));
+        assertEquals(Arrays.asList("valOne", "valTwo", "valThree"), sift.readStringList(data, "stringArray"));
+
+        HashMap<String, Object> parsedInnerMap1 = sift.readHashMap(data, "innerMap");
+
+        assertEquals(innerMap1, parsedInnerMap1);
+        assertEquals("inner1Val", sift.readString(parsedInnerMap1, "innerString"));
+        assertEquals(Arrays.asList(1.2, 32.3, 32.4423), sift.readNumberList(parsedInnerMap1, "innerDoubleArray"));
+
+        List<HashMap<String, Object>> parsedMapList = sift.readHashMapList(data, "mapList");
+        assertEquals(Arrays.asList(arrayMap1, arrayMap2, arrayMap3), parsedMapList);
+        assertEquals("aMS1", sift.readString(parsedMapList.get(0), "arrayMap1String"));
+        assertEquals(3, sift.readNumber(parsedMapList.get(1), "arrayMap2Int"));
+        assertEquals(Arrays.asList("a", "b", "c"), sift.readStringList(parsedMapList.get(2), "arrayMap3Array"));
+
+
     }
 
 }
